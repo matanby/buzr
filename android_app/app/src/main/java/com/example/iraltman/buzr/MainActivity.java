f403ba3f8f1df7b97964d56b4941bb933548d97b
Iris Altman <irisalt21@gmail.com>
2016-05-19 13:37:09 +0300

    Start the navigation bar

--- android_app/app/src/main/java/com/example/iraltman/buzr/MainActivity.java
+++ android_app/app/src/main/java/com/example/iraltman/buzr/MainActivity.java
@@ -80,17 +80,25 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_hot_deals) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.fashion) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.shoes) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.accessories) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.home_furnishings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.beauty_salons_health) {
+
+        } else if (id == R.id.electronics) {
+
+        }  else if (id == R.id.food_and_drinks) {
+
+        } else if (id == R.id.all_stores) {
+
+        } else if (id == R.id.map) {
 
         }
 

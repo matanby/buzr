2a64016952ff881b78b10fd7fd28cd92d4e531f7
Iris Altman <irisalt21@gmail.com>
2016-05-20 03:45:30 +0300

    not working bar commit

--- android_app/app/src/main/java/com/example/iraltman/buzr/MainActivity.java
+++ android_app/app/src/main/java/com/example/iraltman/buzr/MainActivity.java
@@ -102,7 +102,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_shop) {
             return true;
         }
 
